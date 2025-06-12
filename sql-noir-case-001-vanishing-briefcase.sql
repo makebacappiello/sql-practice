aea3c4212b2160d70ac7b839381db728c45c74f0
Makebaliz <makebacappiello@gmail.com>
2025-06-12 17:07:22 -0400

    Update sql-noir-case-001-vanishing-briefcase.sql

--- sql-noir-case-001-vanishing-briefcase.sql
+++ sql-noir-case-001-vanishing-briefcase.sql
@@ -7,3 +7,14 @@
 
 SELECT * FROM suspects;
 began game
+Vincent Malone
+SELECT * FROM suspects
+--WHERE attire = 'trench coat'
+--AND scar = 'left cheek';
+
+--3	Frankie Lombardi	trench coat	left cheek
+--183	Vincent Malone	trench coat	left cheek
+
+--SELECT * FROM interviews
+--WHERE suspect_id = 3
+--OR suspect_id = 183;
